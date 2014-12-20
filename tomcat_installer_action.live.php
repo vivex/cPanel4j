fc50465ed78b9452b71304cc863c3a8ccbd91841
vivek soni <contact@viveksoni.net>
2014-12-20 23:19:28 +0530

    test

--- tomcat_installer_action.live.php
+++ tomcat_installer_action.live.php
@@ -13,10 +13,8 @@ $tomCatVersion = $_POST['tomcat-version'];
 if(($tomCatVersion=='7.0.57' || $tomCatVersion=='8.0.15') & $domainName != ""){
 
     $domainListApiCall = $cpanel->api2('DomainLookup','getdocroot', array() );
-    var_dump($domainListApiCall);
-    echo"<hr>";
     $domainList = $domainListApiCall['cpanelresult']['data'];
-    var_dump($domainList);
+    $domainList = $domainList['0'];
     $docRoot = $domainList['docroot'];
     var_dump($docRoot);
     $roots = explode("/",$docRoot);
