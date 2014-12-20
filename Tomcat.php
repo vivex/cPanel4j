f443469140305f4bbafb0fa685d885ebc94a4790
vivek soni <contact@viveksoni.net>
2014-12-20 23:09:13 +0530

    test

--- Tomcat.php
+++ Tomcat.php
@@ -66,6 +66,8 @@ class Tomcat {
     
     public function createInstance($domainName, $userName, $tomcatVersion) {
         $instancesArray = $this->getXMLArray();
+        echo "INstance Array is ";
+        var_dump($instancesArray);
         $reservedArray = $this->reservedArray($instancesArray);
         //check if  domain already exists exists in instances
         if ($this->checkDomain($instancesArray, $domainName)) {
