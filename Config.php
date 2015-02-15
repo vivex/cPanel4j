ce04cf51408c8387d20d0b8b6a8f46c6fdab119e
Vivek Soni <vivek.soni@costrategix.com>
2015-02-15 18:08:57 +0530

    test

---
+++ Config.php
@@ -0,0 +1,13 @@
+<?php
+//this file should be unEncrypted
+class Config{
+
+
+protected $host= "localhost";
+protected $userName="root";
+protected $password="root";
+protected $database="cpanel4j";
+protected $java_home="/usr/local/jdk/bin/java";
+}
+
+?>
