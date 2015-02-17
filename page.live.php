06eb3f87b05a20cd4b1ca3b43775ff98e6c3e246
Vivek Soni <vivek.soni@costrategix.com>
2015-02-17 23:42:47 +0530

    improvement

--- page.live.php
+++ page.live.php
@@ -20,7 +20,7 @@ if ($action == "list") {
     $count = 1;
     $pending_flag=0;
     $instanceResult = $DBWrapper->getTomcatInstancesByUser($userName);
-    echo "<table class='table'><tr><th>#</th><th>Domain Name</th><th>Path</th><th>TomcatVersion</th><th>Status</th><th>Create Date</th><th>Ports</th><th>Action</th></tr>";
+    echo "<table class='table'><tr><th>#</th><th>Domain Name</th><th>TomcatVersion</th><th>Status</th><th>Create Date</th><th>Ports</th><th>Action</th></tr>";
     if (mysql_num_rows($instanceResult) <= 0)
         echo "<tr><td colspan=7><center>No Instance Yet <a href=page.live.php?action=create_instance>Create One</a></center></td></tr>";
     while ($row = mysql_fetch_array($instanceResult)) {
@@ -44,7 +44,7 @@ if ($action == "list") {
         
         $path="/home/".$row['user_name']."/".$row['domain_name']."/tomcat-".$row['tomcat_version'];
 
-        echo "<tr><td>$count</td><td>" . $row['domain_name'] . "</td>" ."<td>".$path."</td>". "<td>" . $row['tomcat_version'] . "</td><td>$status</td><td>" . $row['create_date'] . "</td><td>ShutDown Port:" . $row['shutdown_port'] . "<br/>HTTP Port:" . $row['http_port'] . "<br/>AJP Port:" . $row['ajp_port'] . "</td><td>";
+        echo "<tr><td>$count</td><td>" . $row['domain_name']."<br/>".$path . "</td>" . "<td>" . $row['tomcat_version'] . "</td><td>$status</td><td>" . $row['create_date'] . "</td><td>ShutDown Port:" . $row['shutdown_port'] . "<br/>HTTP Port:" . $row['http_port'] . "<br/>AJP Port:" . $row['ajp_port'] . "</td><td>";
         if ($row['status'] == "stop")
             echo "<a href=# class='btn btn-success'  onclick='startTomcatInstance(" . $row['id'] . ")'>Start</a> |";
         if ($row['status'] == "start")
