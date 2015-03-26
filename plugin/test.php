9143b74c349fcd5744607b50d007e2700162a698
Vivek Soni <vivek.soni@costrategix.com>
2015-03-26 16:55:34 +0530

    restructured

--- plugin/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Test.php
-//error_reporting(E_ALL);
-//ini_set('display_errors', 1);
-include "DBWrapper.php";
-include "Tomcat.php";
-
-$tomcat = new Tomcat();
-$result = $tomcat->createInstance("VivekSoni.com", "vivek", "1");
